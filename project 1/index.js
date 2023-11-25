65962248dd1b5e5a500d7184d536d78d1e305aac
Barr <if22.akbarhidayat@mhs.ubpkarawang.ac.id>
2023-11-25 11:47:18 +0700

    pertama

---
+++ project 1/index.js
@@ -0,0 +1,24 @@
+let mhs = [
+    [ 1, ' Lenee Lardeur ',' llardeur0@cnbc.com',true ], 
+    [ 2, ' Vivien Woodier ',' vwoodier1@taobao.com',true ], 
+    [ 3, ' Nicolle Risdall ',' nrisdall2@salon.com',true ], 
+    [ 4, ' Marlowe Keener ',' mkeener3@nhs.uk',true ], 
+    [ 5, ' Terrance McDoual ',' tmcdoual4@examiner.com',true ], 
+    [ 6, ' Gordie Chill ',' gchil5@purevolume.com',true ], 
+    [ 7, ' Pascal Clymo ',' pclymo6@indiatisme.com',true ], 
+    [ 8, ' Janith Strute ',' jstrute7@icq.com',false ], 
+    [ 9, ' Isabella Pendreigh ',' ipendreigh8@apple.com',true ], 
+    [ 10, ' Kizzee Gareisr ',' kgareisr9@youtu.be',false ], 
+]
+
+console.log(mhs[0])
+console.log(mhs[1])
+console.log(mhs[2])
+console.log(mhs[3])
+console.log(mhs[4])
+console.log(mhs[5])
+console.log(mhs[6])
+console.log(mhs[7])
+console.log(mhs[8])
+console.log(mhs[9])
+console.log(mhs[10])
